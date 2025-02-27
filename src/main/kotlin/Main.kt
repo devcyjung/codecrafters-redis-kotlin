a1888ffb940e505109a499faf3c6d064c43a103e
chiyoung jung <jcy0011@yahoo.com>
2025-02-27 15:56:29 +0900

    PONG message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,18 @@
 import java.net.ServerSocket
 
-fun main(args: Array<String>) {
-    val serverSocket = ServerSocket(6379)
+const val PORT = 6379
+const val PONG_MESSAGE = "+PONG\r\n"
 
-    // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    // ensures that we don't run into 'Address already in use' errors
+fun main(args: Array<String>) {
+    val serverSocket = ServerSocket(PORT)
     serverSocket.reuseAddress = true
     println("server started at port 6379")
 
     // Wait for connection from client.
-    serverSocket.accept()
-    println("accepted new connection")
+    val clientSocket = serverSocket.accept()
+    println("accepted new connection from ${clientSocket.remoteSocketAddress}")
+
+    val output = clientSocket.getOutputStream()
+
+    output.write(PONG_MESSAGE.toByteArray())
 }
